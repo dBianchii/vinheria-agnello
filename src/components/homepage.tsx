a8e35bb09145eca432917c73ee86a775aaed4eed
victobonetti <victorbonettixd@gmail.com>
2024-09-24 14:26:42 -0300

    add counter to homepage

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -5,8 +5,56 @@ import CardHomePage from "./card-home";
 import Header from "./header";
 import Footer from "./footer";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 
 export function Homepage() {
+  const [counterViniculas, setCounterViniculas] = useState(0);
+  const [counterRotulos, setCounterRotulos] = useState(0);
+  const [counterClientes, setCounterClientes] = useState(0);
+
+  const limits = {
+    viniculas: 50,
+    rotulos: 1000,
+    clientes: 10000,
+  };
+
+  useEffect(() => {
+    const updateCounters = () => {
+      setCounterViniculas((prevViniculas) => {
+        if (prevViniculas < limits.viniculas) {
+          return Math.min(prevViniculas + 1, limits.viniculas);
+        }
+        return prevViniculas;
+      });
+
+      setCounterRotulos((prevRotulos) => {
+        if (prevRotulos < limits.rotulos) {
+          return Math.min(prevRotulos + 20, limits.rotulos);
+        }
+        return prevRotulos;
+      });
+
+      setCounterClientes((prevClientes) => {
+        if (prevClientes < limits.clientes) {
+          return Math.min(prevClientes + 200, limits.clientes);
+        }
+        return prevClientes;
+      });
+    };
+
+    const calculateInterval = () => {
+      const viniculasProximity = (counterViniculas - limits.viniculas);
+    
+      // As viniculasProximity decreases (closer to the limit), the interval decreases
+      return 100 + (2 * viniculasProximity); // Starts faster and slows down near the limit
+    };
+    
+
+    const interval = setInterval(updateCounters, calculateInterval());
+
+    return () => clearInterval(interval);
+  }, [counterViniculas, counterRotulos, counterClientes]);
+
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Header */}
@@ -47,15 +95,15 @@ export function Homepage() {
       <section className="bg-white py-12">
         <div className="container mx-auto flex justify-center space-x-8 px-4 md:space-x-16">
           <div className="text-center">
-            <h3 className="text-3xl font-bold">50+</h3>
+            <h3 className="text-3xl font-bold">{counterViniculas}+</h3>
             <p className="text-gray-600">Vinícolas Selecionadas</p>
           </div>
           <div className="text-center">
-            <h3 className="text-3xl font-bold">1.000+</h3>
+            <h3 className="text-3xl font-bold">{counterRotulos}+</h3>
             <p className="text-gray-600">Rótulos exclusivos</p>
           </div>
           <div className="text-center">
-            <h3 className="text-3xl font-bold">10.000+</h3>
+            <h3 className="text-3xl font-bold">{counterClientes}+</h3>
             <p className="text-gray-600">Clientes Satisfeitos</p>
           </div>
         </div>
