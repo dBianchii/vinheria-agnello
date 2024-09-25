0b886fc432cce1c8ef9fd40428b7812d65559199
wwwacedo <eng.marianammacedo@gmail.com>
2024-09-25 07:41:29 -0300

    adicionando funcões formatPrice e calculatePriceAfterDiscount

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(price: number) {
+	return price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+}
+
+export function calculatePriceAfterDiscount(price: number, discount: number) {
+	return price - (price * discount) / 100
+}
