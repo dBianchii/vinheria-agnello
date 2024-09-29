0f16d307ee28225e001286e02953162bf1ad0726
victobonetti <victorbonettixd@gmail.com>
2024-09-29 20:31:08 -0300

    wip

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,7 +3,8 @@ import { ProductPageComponent } from "~/components/product-page";
 import { getWineById, getWines } from "~/server/db/select";
 
 export default async function ProductPage({ params }: { params: { id: number } }) {
-  const product = await getWineById(params.id)
-  if (!product) return notFound();
-  else return <ProductPageComponent product={product[0]} />;
+  const product = await getWineById(params.id);
+  if (!product || product.length === 0 || !product[0]) return notFound();
+  return <ProductPageComponent product={product[0]} />;
 }
+
