7bc9ed4608b56694e16b425512bed77c0fd25015
Gabriel Bianchi <gdbianchii@gmail.com>
2024-09-30 18:47:41 -0300

    move getCountryImage

--- src/components/product-page/index.tsx
+++ src/components/product-page/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { getCountryImg } from "data/getCountryImg";
 import { Archive, Wine } from "lucide-react";
 import Image from "next/image";
 import { useEffect } from "react";
@@ -79,4 +78,30 @@ export function ProductPageComponent({
       </div>
     </div>
   );
+}
+
+function getCountryImg(country: string): string {
+  let link = "https://cdn-icons-png.flaticon.com/512/9746/9746676.png";
+  switch (country.toLowerCase()) {
+    case "brasil":
+      link = "https://cdn-icons-png.flaticon.com/128/5372/5372653.png";
+      break;
+    case "argentina":
+      link = "https://cdn-icons-png.flaticon.com/128/197/197573.png";
+      break;
+    case "portugal":
+      link = "https://cdn-icons-png.flaticon.com/128/12364/12364368.png";
+      break;
+    case "espanha":
+      link = "https://cdn-icons-png.flaticon.com/128/197/197593.png";
+      break;
+    case "frança":
+      link = "https://cdn-icons-png.flaticon.com/128/197/197560.png";
+      break;
+    case "italia":
+      link = "https://cdn-icons-png.flaticon.com/128/197/197626.png";
+      break;
+  }
+  console.log(link);
+  return link;
 }
