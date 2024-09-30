d8515bc9ff75561e77ea175572cc4f034271e844
Gabriel Bianchi <gdbianchii@gmail.com>
2024-09-30 08:14:51 -0300

    lazy loading not necessary lmao

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,6 @@
-import dynamic from "next/dynamic";
+import ProductsPage from "~/components/ui/products/products-page";
 import { getWines } from "~/server/db/select";
-
-const ProductsPageComponent = dynamic(
-  () => import("~/components/ui/products/products-page"),
-  { ssr: false, loading: () => <p>Loading...</p> },
-);
-
 export default async function CartPage() {
-	const wines = await getWines();
-  return <ProductsPageComponent wines={wines}/>;
+  const wines = await getWines();
+  return <ProductsPage wines={wines} />;
 }
-
