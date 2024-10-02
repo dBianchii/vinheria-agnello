ab31f6e1e580eb804735fb38e74b5aa45d9a0e4c
wwwacedo <eng.marianammacedo@gmail.com>
2024-10-02 07:49:31 -0300

    adicionando tipo do retorno

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,10 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatPrice(price: number) {
+export function formatPrice(price: number): string {
   return price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
 
-export function calculatePriceAfterDiscount(price: number, discount: number) {
+export function calculatePriceAfterDiscount(price: number, discount: number): number {
   return price - (price * discount) / 100;
 }
