81e9cf5fa0c7a09d444ed82dd663716d4a0ffd15
wwwacedo <eng.marianammacedo@gmail.com>
2024-10-06 10:38:30 -0300

    style Button onClose modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,5 +1,7 @@
 import { useEffect, type ReactNode } from "react";
 import { createPortal } from "react-dom";
+import { Button } from "./button";
+import { X } from "lucide-react";
 
 interface ModalProps {
   onClose: () => void;
@@ -19,12 +21,14 @@ export default function Modal({ onClose, children }: ModalProps) {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative w-full max-w-lg rounded bg-white p-6 shadow-lg">
         {/* Botão para fechar o modal */}
-        <button
-          className="absolute right-2 top-2 text-gray-600 hover:text-gray-800"
+        <Button
+          className="absolute right-2 top-2"
           onClick={onClose}
+					variant={"ghost"}
+					size={"icon"}
         >
-          Fechar
-        </button>
+          <X />
+        </Button>
         {/* Conteúdo do modal */}
         {children}
       </div>
